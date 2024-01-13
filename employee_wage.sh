a9e9ead5986bd6ada7e592ea0509edf0b81c29aa
mk3709 <mk3709@srmist.edu.in>
2024-01-13 12:59:14 +0530

    feauture added

--- employee_wage.sh
+++ employee_wage.sh
@@ -13,3 +13,12 @@ full_day_hours=8
 daily_wage=$((wage_per_hour*full_day_hours))
 echo  $daily_wage
 
+
+
+
+part_time=8
+wage_per_hour=20
+daily_wage=$((part_time*wage_per_hour))
+echo $daily_wage
+
+
